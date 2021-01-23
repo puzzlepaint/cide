7c747fe0961d51178b381ca4365cd0b53784496e
Thomas Schöps <tom.schoeps@gmail.com>
2021-01-23 15:05:12 +0400

    Specify Ctrl+W as default shortcut to close the current document instead of using Qt's close shortcut, because that would be Ctrl+F4 on Windows

--- src/cide/settings.cc
+++ src/cide/settings.cc
@@ -281,7 +281,7 @@ Settings::Settings() {
   AddConfigurableShortcut(tr("Open file"), openFileShortcut, QKeySequence::Open);
   AddConfigurableShortcut(tr("Save file"), saveFileShortcut, QKeySequence::Save);
   AddConfigurableShortcut(tr("Save file as..."), saveAsFileShortcut, QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_S));
-  AddConfigurableShortcut(tr("Close file"), closeFileShortcut, QKeySequence::Close);
+  AddConfigurableShortcut(tr("Close file"), closeFileShortcut, QKeySequence(Qt::CTRL + Qt::Key_W));
   AddConfigurableShortcut(tr("Quit program"), quitShortcut, QKeySequence());
   AddConfigurableShortcut(tr("Find and replace in files"), findAndReplaceInFilesShortcut, QKeySequence(Qt::CTRL + Qt::ALT + Qt::Key_F));
   AddConfigurableShortcut(tr("Show project files dock"), showProjectFilesDockShortcut, QKeySequence());
